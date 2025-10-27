72c7490451ff2a1cd3b8e0fc98eedd915cd87ddc
nicolaslimadev <nicolaslima.dev.web@gmail.com>
2025-10-27 16:38:34 -0300

    feat: Adicionando funcionalidade do formulário, useState e criando botão e função de carregar mais.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,34 @@
+import { useState, type FormEvent } from "react";
 import styles from "./home.module.css";
 import { BsSearch } from "react-icons/bs";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export function Home() {
+  const [input, setInput] = useState("");
+
+  const navigate = useNavigate()
+
+  function handleSubmit(e: FormEvent){
+    e.preventDefault()
+
+    if(input === "") return;
+
+    navigate(`/detail/${input}`)
+  }
+
+  function handleGetMore(){
+    alert('Funcionando!')
+  }
+
   return (
     <main className={styles.container}>
-      <form className={styles.form}>
-        <input type="text" placeholder="Digite o nome da moeda... EX bitcoin" />
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input 
+          type="text"
+          placeholder="Digite o nome da moeda... EX bitcoin"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
         <button type="submit">
           <BsSearch size={30} color="#FFF" />
         </button>
@@ -52,6 +74,10 @@ export function Home() {
           </tr>
         </tbody>
       </table>
+
+      <button onClick={handleGetMore} className={styles.buttonMore}>
+        Carregar mais
+      </button>
     </main>
   );
 }
